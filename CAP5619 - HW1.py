4f2809b0aea4ac2a971f1d490a092f7fa437d0a7
Farez Siddiqui <43183190+fmsiddi@users.noreply.github.com>
2024-02-13 00:43:00 -0500

    finished problem 1
    finished problem 1

--- CAP5619 - HW1.py
+++ CAP5619 - HW1.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from numpy import exp as exp
 plt.style.use('seaborn')
 
 # relu = lambda x: x if x >= 0 else 0
@@ -9,26 +10,48 @@ plt.style.use('seaborn')
 # elu = lambda x: x if x >= 0 else .05*(np.exp(x)-1)
 
 # x = np.linspace(-5,5,101)
-# plt.plot(x, np.gradient([relu(i) for i in x]))
-# plt.plot(x, np.gradient([sigmoid(i) for i in x]))
-# plt.plot(x, np.gradient([pwlu(i) for i in x]))
-# plt.plot(x, np.gradient([swish(i) for i in x]))
-# plt.plot(x, np.gradient([elu(i) for i in x]))
+# plt.scatter(x, np.gradient([relu(i) for i in x]))
+# plt.scatter(x, np.gradient([sigmoid(i) for i in x]))
+# plt.scatter(x, np.gradient([pwlu(i) for i in x]))
+# plt.scatter(x, np.gradient([swish(i) for i in x]))
+# plt.scatter(x, np.gradient([elu(i) for i in x]))
 
 def relu_grad(x):
-    # x: an array of input values
-    out = x
-    out[out >= 0] = 1
-    out[out < 0] = 0
-    return out
-
-input = np.array([-1,0,1,2])
-
-print(relu_grad(input))
+    return np.array([1 if i >= 0 else 0 for i in x])
 
 def sigmoid_grad(x):
-    return np.array([np.exp(-i)/((1 + np.exp(-i))**2) for i in x])
-
-input = np.linspace(-5,5,101)
-plt.plot(input,sigmoid_grad(input))
+    return np.array([exp(-i)/((1 + exp(-i))**2) for i in x])
     
+def pwlu_grad(x):
+    return np.array([1 if abs(i) <= 1 else .1 for i in x])
+
+def swish_grad(x):
+    return np.array([exp(2.5*i) * (exp(2.5*i) + 5*i + 2)/(2*(exp(2.5*i) + 1)**2) for i in x])
+
+def elu_grad(x):
+    return np.array([1 if i >= 0 else .05*exp(i) for i in x])
+
+def plot_gradient(x, grad_func, title):
+    out = grad_func(x)
+    inactive = np.where(np.abs(out) == 0.0)[0]
+    slow = np.where((np.abs(out) > 0) & (np.abs(out) <= 0.1))[0]
+    active = np.where((np.abs(out) >= 0.1) & (np.abs(out) <= 0.99))[0]
+    fast = np.where(np.abs(out) > 0.99)[0]
+    out = np.array(out)
+    plt.scatter(x[inactive], out[inactive], label='Inactive_learning', s=5)
+    plt.scatter(x[slow], out[slow], label='Slow_learning', s=5)
+    plt.scatter(x[active], out[active], label='Active_learning', s=5)
+    plt.scatter(x[fast], out[fast], label='Fast_learning', s=5)
+    plt.ylabel("Gradient Value")
+    plt.xlabel("Input Value")
+    plt.title(title)
+    plt.legend(loc='best')
+    plt.show()
+    
+if __name__ == "__main__":
+    x = np.linspace(-5,5,1001)
+    plot_gradient(x, relu_grad, 'Gradient of ReLU')
+    plot_gradient(x, sigmoid_grad, 'Gradient of Sigmoid')
+    plot_gradient(x, pwlu_grad, 'Gradient of PwLU')
+    plot_gradient(x, swish_grad, 'Gradient of Swish')
+    plot_gradient(x, elu_grad, 'Gradient of ELU')
